95cf783af4d612b2ab277719f76188457605a1a1
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-25 23:58:30 +0600

    simple operation in js

---
+++ operation.js
@@ -0,0 +1,25 @@
+//operation using javascript 
+var onionPrice=20;
+var eggPrice =30;
+
+var orangePrice=20;
+var quantity=7;
+
+var PriceDifference =onionPrice -eggPrice; // substraction here
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log("Total Price of onion and egg",onionPrice +eggPrice);// addition operation here
+
+console.log("Price Difference between onion and Egg =",PriceDifference);
+
+var totalCost =orangePrice*quantity;// Multiplication 
+
+console.log("Total price of orange",totalCost); 
+
+//division 
+var money=500;
+var player =10;
+
+var eachPlayer=money/player;
+console.log("Each player get=",eachPlayer);
