b30920e29a21c7e5250cb35283fd1ec58a9090d1
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-25 23:45:50 +0600

    javascript variable naming rules

---
+++ variableName.js
@@ -0,0 +1,38 @@
+/****
+ * variable naming in js
+ * 
+ *  
+ * 
+ */
+// good Variable
+var price =30;//var is keyword or reserved word
+// 1.variable can't be keyword or reserved word
+
+var my Home Address ="new California";
+// 2. Variable name has to be one word that means no specing
+
+var "name"="Abdullah";
+
+// 3.Variable name can not a quotation.
+
+var 99club=1971;
+//4.variable name can't start with number,but ends with a  number;
+
+var club99=1971 // right variable
+
+// 5.How to use long name 
+// can't use dash
+var user-name="habibi";
+
+var userHomeAddress ="Mirpur-02,Dhaka";
+// camelCase formate
+var where_go="Dhaka"; // snake_case
+//Can use _(underscore),but mostly people are use camelCase format in javascript world.
+var DreamCountry ="United States of America";// Pascal Case
+
+// Variable name is Case Sensitive in js
+var number=1;
+var Number=10;// These are two different variables
+
+
+// * we will use camelCase
