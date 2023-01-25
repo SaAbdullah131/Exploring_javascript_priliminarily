11123c39e41073e150e43acf96ae3ba9352622e2
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-26 01:07:44 +0600

    type checking using typeof function and reminder or modules operator in js

---
+++ reminder.js
@@ -0,0 +1,11 @@
+// Reminder in js
+
+var mangoes =7;
+var person =2;
+
+// /->this means Division result
+
+// % ->This means Reminder after division -modulus operator 
+
+console.log(mangoes/person);// 3.5
+console.log(mangoes%person);//1
---
+++ type.js
@@ -0,0 +1,22 @@
+// variable types using typeof function in js 
+
+var price =100;
+console.log(typeof(price));
+
+var price1=100.33;
+console.log(typeof(price1));
+
+var name="SA Abdullah";
+console.log(typeof( name));
+
+var isHappy=false;
+console.log(typeof isHappy);
+
+var romantic ;
+console.log(typeof(romantic));
+
+var num1=0.1;
+var num2=0.2;
+sum=num1+num2;
+sum=sum.toFixed(4);
+console.log(sum);
