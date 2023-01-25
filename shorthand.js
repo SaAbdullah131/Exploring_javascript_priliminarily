449b2ec2000c83f9821630eab501e0ecd81e9270
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-26 00:34:55 +0600

    Shorthands of operator in js

---
+++ shorthand.js
@@ -0,0 +1,31 @@
+// variable declaration
+
+var tomatoPrice =25;
+// variable value update
+tomatoPrice=35;
+
+// add something to the existing value
+var price1=31;
+var price2=10;
+var price3=5;
+var price4=20;
+var price5=23;
+
+// price1=price1+60;
+
+price1+=60; // price1=price1+60;shorthand
+
+// price2=price2-5;
+
+price2-=5;//shorthands 
+// -=menas deduction some values from the current values of the variable 
+
+price3*=5;// multiplcation shorthand
+price4/=3;// division shorthand
+price5%=3;// reminder shorthand 23/3 =2 reminder 2.
+
+console.log(price1);
+console.log(price2);
+console.log(price3);
+console.log(price4);
+console.log(price5);
