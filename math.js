d7370325fe54018351c27833b4595f9f122436b1
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-25 20:55:41 +0600

    Hello World in javascript

---
+++ math.js
@@ -0,0 +1,12 @@
+/******
+ * 1.Number ==> 55, 55.8 -32;
+ * 2.Integer ==> 2,34,5;
+ * 3.Floating Number ==> 1.2, 55.4454;
+ * 4.Operation : addition ,substraction,multiplication,division, reminder 
+ * 5.percent
+ * 6.Average
+ * 7.Comparison :>,<,==,!=,>=,<=;
+ * 8.oikik: 2 kilo apple 9 kilo mango .
+ * 45,243,243043.. sum,sub,avg,
+ * 9.Series :
+ */
