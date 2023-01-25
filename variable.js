1de339e517be1c762bfb4c09ce8e043c31f3a0eb
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-25 23:25:04 +0600

    string and boolean types variable

--- variable.js
+++ variable.js
@@ -1,10 +1,20 @@
 // very+able =variable ..Changeable 
 
+// numeric variable
 var price = 13;
 var age=23;
 var temperature =22.5;
 var myHandMoney=220;
 var myMessVara =14000;
+//string variable 
+var person ="SA Abdullah";
+var myLocation="Mirpur-02,Dhaka";
+var special ="Saiyee";
+
+// boolean variable true or false
+    var serious= true;
+    var isFullMark= true;
+    var isSingle=false;
 
 //print variable values 
 
@@ -13,5 +23,7 @@ console.log(age);
 console.log(temperature);
 console.log(myHandMoney);
 console.log(myMessVara);
+console.log(person);
+console.log(serious);
 
 
