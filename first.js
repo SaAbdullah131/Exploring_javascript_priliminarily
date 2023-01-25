ddecaff5457be21dd8291930d1327c271cda039b
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-25 21:11:20 +0600

    variable naming styles in js

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
 console.log(7);
 console.log ("hello World");
 console.log ("Hello Javascript.I'm Your World");
+console.log("Javascript Invented Brendan Eich in 1995");
---
+++ variable.js
@@ -0,0 +1,17 @@
+// very+able =variable ..Changeable 
+
+var price = 13;
+var age=23;
+var temperature =22.5;
+var myHandMoney=220;
+var myMessVara =14000;
+
+//print variable values 
+
+console.log (price);
+console.log(age);
+console.log(temperature);
+console.log(myHandMoney);
+console.log(myMessVara);
+
+
