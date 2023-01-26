ed7f109c3df518e68954280d90fd74ed57ef746f
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 01:17:40 +0600

    comparison in js

---
+++ Array_and_Conditional/comparison.js
@@ -0,0 +1,25 @@
+// comparison in javascript ..
+
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5==6);
+console.log(5==5);
+console.log(5!=5);
+// Less than or equal
+console.log(8<=8);
+console.log(16<=6);
+
+//less than or equal to 
+console.log(5>=2);
+console.log(16>=20);
+
+var num1=56;
+var num2=89;
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1<=nym2);
+
+// multiple condition  
+//  money1>money2 && result1>result2;both should true.
+// money1> money2 || result1>result2;one condition true it works..
