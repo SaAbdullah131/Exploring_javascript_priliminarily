30a85bce401783ecc0a98b79f4aa12f0ce110c4a
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 01:28:16 +0600

    conditional statement in js

---
+++ Array_and_Conditional/conditional.js
@@ -0,0 +1,24 @@
+var iphonePrice =79000;
+var myBudget=10500;
+
+/**
+ * if iphone price is less than my budget.I will buy the iphone
+ * 
+ * if (condition){
+ *     // will execute if           the         condition if true}
+ */
+
+
+if(iphonePrice<myBudget){
+    console.log("I will buy the Iphone");
+}else {
+    console.log("I will not buy ");
+}
+var chickenPrice=500;
+var myMoney=499;
+
+if(chickenPrice<myMoney){
+    console.log("I will be going to eat");
+}else {
+    console.log("I will eat samsesh photo and lentil soup");
+}
