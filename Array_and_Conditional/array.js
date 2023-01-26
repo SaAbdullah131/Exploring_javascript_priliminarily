2b42265f966a6bf04d8109560c289562a697c806
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 00:01:54 +0600

    Array in javascript

---
+++ Array_and_Conditional/array.js
@@ -0,0 +1,11 @@
+// Array in javascript.. collection of data 
+var friendsAge=[11,21,45,17,14,105,06];
+
+var picnicFees=[1000,2000,3400,4500,500];
+
+var nayikas =["mahi","Opu","Saila Sabi","Sabana"];
+var vowel=["a","e","i","o","u"];
+
+
+console.log(friendsAge.length);
+console.log(nayikas[0]);
