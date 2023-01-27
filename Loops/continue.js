ab1da604c2a014c3e5c3fee9dbf5689c0b0e99fd
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-28 00:29:07 +0600

    reverse way for and while loop in js

--- Loops/continue.js
+++ Loops/continue.js
@@ -1,6 +1,7 @@
 // continue statement in js...
 
 var numbers=[45,87,89,56,32,51,25,188,41,25,98];
+
 for(var i=0;i<numbers.length;i++){
     var number=numbers[i];
     if(numbers[i]>50){
---
+++ Loops/reverse.js
@@ -0,0 +1,20 @@
+// reversing process in js
+
+var num=1;
+while(num<=10){
+    console.log(num);
+    num++;
+}
+// while loop reverse way
+console.log("10 to 1\n");
+var num=10;
+while(num >= 1){
+    console.log(num);
+    num--;
+}
+
+//reverse way to for loop 
+
+for (var i=10;i>=1;i--){
+    console.log(i);
+}
