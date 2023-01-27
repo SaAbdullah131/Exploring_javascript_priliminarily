bad8e95dc964bc66e5e61cf8f82b6fb4e9efd882
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 21:42:38 +0600

    for loop in js

---
+++ Loops/for.js
@@ -0,0 +1,22 @@
+// for loop in js
+
+for( var roastGiven=0;roastGiven<7;roastGiven++){
+    console.log(roastGiven,"Roast Den. Please!!");
+}
+
+// simple version of for loop 
+console.log("Simple For Loop");
+
+for (var i=0;i<7;i++){
+    console.log(i);
+}
+console.log("1 to 10");
+
+for(var i=1;i<=10;i++) {
+    console.log(i);
+}
+// All even number 0 to 20;
+console.log("Even number from 0 to 20");
+for(var i=0;i<=20;i+=2){
+    console.log(i);
+}
