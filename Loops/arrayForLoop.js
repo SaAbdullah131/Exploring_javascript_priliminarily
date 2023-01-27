50a81d16543b4117d4298dc3b655db667e9091b8
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 21:55:52 +0600

    run for loop of each element of array in js

---
+++ Loops/arrayForLoop.js
@@ -0,0 +1,16 @@
+// Run a loop for each of an array element
+
+var numbers=[45,87,89,56.32,51,25,188,41];
+numbers.push(89);
+
+for(var i=0;i<numbers.length;i++){
+    var number=numbers[i];
+    console.log(number);
+}
+
+var items=["bottle","mouse","sungless","pen"];
+items.push("Labu");
+for(var i=0;i<items.length;i++){
+    // var item=items[i];
+    console.log(items[i]);
+}
