0fb39623a0c5e32b043dfb84247d16f1bb2e9033
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-29 16:35:42 +0600

    Recap js variable

--- Recap_of_Variable_Array_Loop.js/variable.js
+++ Recap_of_Variable_Array_Loop.js/variable.js
@@ -1,13 +1,25 @@
 /** 
  * Variable 
- * 
+ * 1.What is Javascript 
+ * Ans:Javascript in high level programming language..It's a scripting language for web pages..JavaScript (JS) is a     lightweight, interpreted, or just-in-time compiled programming language with first-class functions.
+ * 2.How Does Javascript Works?
+ * Ans:an interpreter in the browser reads over the JavaScript code, interprets each line, and runs it. More modern browsers use a technology known as Just-In-Time (JIT) compilation, which compiles JavaScript to executable bytecode just as it is about to run.
+ * 3.What is Variable?
+ * Ans:Variable are like's container.Which Store the Values
+ * 4.Declare a Variable?
+ * Ans: var name ="SA Abdullah";
+ * 5.Types Of Variable? How Can you find out types of variable?
+ * Ans.Primitive types: numeric ,string,null,boolen,undefined; non-primitive types: Array,Function,Object;
+ * 6.Primitive and non-primitive data types 
+ *Ans
+ 7.Naming Convention os Js Variable?
+ Ans:camelCase is the best Practice .. 
+8.Math Operation?
+Ans.+,(additions),-(Substaction),*(multiplication),/(division),%(modulus);
+9.Short Hands?
+Ans:++->means increase value 1 ...-- ->decrease by 1 And a+=1;a=a+1;
+10.ParseInt,PraseFloat?
+Ans.ParseInt for Showing integer...and PraseFloat that means showing floating Number...
+11.toFixed?
+Ans.toFixed for fixed value after integer suppose a=12.2345. when a.toFixed(2)..a=12.23 just
  */
-1.What is Javascript ?
-  Ans:
-2.How Does Javascript Works?
- Ans:
-3.What is Variable?
- Ans:
- 4.Declare a Variable?
- Ans:
- 5.Types Of Variable? How Can you find 
