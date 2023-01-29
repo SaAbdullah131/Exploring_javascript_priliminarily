4173e36781bd71f864d40c66b8ad0efc4bad37f4
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-30 01:12:23 +0600

    apply looping in an object

---
+++ object/objectLooping.js
@@ -0,0 +1,40 @@
+// Looping in Object ..
+// array vs object ..
+
+//one similar information in hold into an array..
+const shoppingItems=["books","sunglass","shoes","pend"];
+const friendsAge=[21,22,20,35,34,27,18];
+const friendsAges={
+    Rion:25,
+    Shakibe:26,
+    Mukter:23,
+    Saidur:22,
+    Khaliq:30
+}
+const shoppingCart ={
+    books:3,
+    sunglass:1,
+    keyboard:5,
+    mouse:1,
+    pen:25,
+    shoes:2,
+}
+
+// keys
+const keys=Object.keys(shoppingCart);
+console.log(keys,"\n");
+// values 
+const values=Object.values(shoppingCart);
+console.log(values);
+
+for(var i=0;i<keys.length;i++){
+    propertyName=keys[i];
+    propertyValue=values[i];
+    console.log(propertyName,propertyValue);
+}
+
+// using for in loop
+for(var propertyName  in shoppingCart){
+    const value=shoppingCart[propertyName]
+    console.log(propertyName,value);
+}
