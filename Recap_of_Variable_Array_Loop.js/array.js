602f05dc13a2d5bfd2826320a47fb6ac3548b3eb
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-29 17:30:54 +0600

    Array recap in js

--- Recap_of_Variable_Array_Loop.js/array.js
+++ Recap_of_Variable_Array_Loop.js/array.js
@@ -1,24 +1,30 @@
 /**
  *  ARRAY
- * 1.Wht is the purpose?
+ * 1.Wht is the purpose of array?
+ * Ans. The main purpose of array in store multiple values just using one variable and same types values in one array in good practice in js.
+ * 
  * 2.How to declare an array in js?
- * Ans.
+ * Ans.var class=["six","seven","Eight","Nine"];
+ * 
  * 3.Number of Element in an Array?
- * Ans.
+ * Ans.length property use for getting length of any specific array
  * 4.What is Index?
- * Ans:
+ * Ans:Index Indicate the position of the element within the array..
+ * 
  * 5.Find the Value of an element by index?
+ * Ans.Array.indexOf(10);// finding 10 values in that array
  * 6.Change an Element by index?
- * Ans.
+ * Ans.array[1]="";
  * 7.Get the index of an element by the value?
- * Ans.
+ * Ans.indexOf..
  * 8.What does it mean when you get undefined while getting the value of an element by index?
- * Ans.
+ * Ans.When Variable has not been assigned a value 
  * 9.How can you add an element to an array at the last position?
- * Ans.
+ * Ans.array.push()
  * 10.How can you remove the last element of an array..
+ * Ans:array.pop();
  * 11.Add an element at the first position of an array?
- * Ans:
+ * Ans:array.unshift();
  * 12.Remove first element of an array?
- * ans:
+ * ans:array.shift();
  */
--- Recap_of_Variable_Array_Loop.js/conditions.js
+++ Recap_of_Variable_Array_Loop.js/conditions.js
@@ -3,9 +3,9 @@
  * CONDITIONALS 
  * ------------
  * 1.Meaning of :>,<,>=,<=,!=,==,===,!==;
- * Ans:
+ * Ans:> less than,< greater than ,>=Less than or equal ,<= greater than or equal.. != not equal ..== check values equal or not... === check value and data types .. !== check values and data types not equal
  * 2.Meaning of : &&;
- * Ans:
+ * Ans: && and means both conditions should be true..
  * 3.Meaning of ||?
- * Ans:
+ * Ans: || or logical operator is execute then any conditions is true when first conditions it doesn't need to check another condition
  */
