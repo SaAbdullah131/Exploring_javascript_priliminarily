c024d90682a0796de4e2e8d0168e94144534834f
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-29 21:32:06 +0600

    function in js

---
+++ Array_and_Conditional/subStringAndInclude.js
@@ -0,0 +1,7 @@
+var into="I'm not a great developer";
+
+into.includes("has");
+var sub=into.substring(0,7);
+console.log(sub);
+
+// array in imituable 
---
+++ function.js/functon.js
@@ -0,0 +1,15 @@
+// function in javascript 
+
+// function declaration
+function startFan(){
+    console.log("Stand up");
+    console.log("Walk toward the switch");
+    console.log("Press the switch");
+}
+
+// cal the function
+startFan();
+console.log("\n");
+startFan();
+console.log("\n");
+startFan();
