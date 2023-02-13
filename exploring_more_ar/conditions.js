b225b56ac49ff155953131c2343a121e6f80c271
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-13 23:45:02 +0600

    do while and while loop

--- exploring_more_ar/conditions.js
+++ exploring_more_ar/conditions.js
@@ -13,3 +13,26 @@ if(teacherSpeaks) {
 if(!teacherSpeaks) {
     shout();
 }
+// scenario 1: single branch 
+// if condition
+if(hasMoney) {
+    purchasePhone();
+}
+
+// scenario 2: Two branch 
+// if else condition
+if(toss === hand) {
+    win();
+}
+else {
+    lose();
+}
+// scenario 3: Multiple Branch
+// else if
+if(1>1){
+    Big();
+}else if(1<1){
+    small();
+}else {
+    same();
+}
---
+++ exploring_more_ar/loop.js
@@ -0,0 +1,23 @@
+// repeat using loop
+// there are mainly three types of loop.
+//1.for(When we know the range)
+//2.while(when we don't know the range ,but know the condition)
+//3.Do while (one iteration should be do wheather the condition false)
+for(let i=1;i<=100;i++){
+    // it's a new js file ..you can write any valid js code here.
+    // Every code written inside this block will execute multiple times 
+    console.log("SA Abdullah");
+}
+
+while(true){
+    let num = Math.ceil(Math.random()*100);
+    if(num===99){
+    break;
+}
+    console.log(num,"SA Abdullah");
+}
+
+do{
+    console.log("It will run at least once ");
+}while(false) ;
+
